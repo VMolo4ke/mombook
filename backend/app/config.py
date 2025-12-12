2380e9e8ca60f55241994b5c489abd8f7390e2cf
Volodya <gavr.22898@gmail.com>
2025-12-13 04:15:39 +0500

    init

---
+++ backend/app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "MomBook"
+    debug: str = True
+    database_url: str = "sqlite:///./shop.db"
+    cors_origins: list = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
+    static_dir: str = "static"
+    image_dit: str = "static/image"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
