52f1753cd5a9673947e811d9a99123d8aad45663
Volodya <gavr.22898@gmail.com>
2025-12-14 05:57:49 +0500

    add min frontend

---
+++ frontend/src/components/Item.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="item">
+    <img class="item_image" src="../assets/image/book.jpg" alt="" />
+    <div class="item__info">
+      <p class="item__price">360 ₽</p>
+      <h3 class="item__name">Книжка-липучка "Домик дед Мороза"</h3>
+      <button class="item__button">В корзину</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  overflow: hidden;
+  background: var(--color-background-soft);
+  border-radius: 15px;
+  padding: 0 0 10px 0;
+}
+.item__info {
+  width: 92%;
+  margin: 0 auto;
+}
+.item__price {
+  color: #ff4294;
+  font-size: 16px;
+  font-weight: 700;
+}
+.item__name {
+  font-size: 15px;
+  line-height: 1.4;
+  flex: 1;
+  margin: 0 0 10px;
+}
+.item__button {
+  background: var(--button-background);
+  color: #fff;
+  font-weight: 600;
+  border: none;
+  width: 100%;
+  height: 30px;
+  border-radius: 5px;
+}
+.item_image {
+  width: 100%;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Item from '@/components/Item.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="header">
+      <input type="text" placeholder="Искать товар" class="header__input" />
+      <div class="header__category">
+        <div class="category__item">Книжки</div>
+        <div class="category__item">Игрушка</div>
+      </div>
+    </header>
+    <main class="main">
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </main>
+  </div>
 </template>
+
+<style scoped>
+.header {
+  width: 95%;
+  padding: 10px 0 0 0;
+  margin: 0 auto 20px;
+}
+.header__input {
+  width: 100%;
+  height: 30px;
+  background: var(--color-background-mute);
+  padding: 0 0 0 10px;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  margin: 0 0 7px 0;
+}
+.header__input:hover {
+  border: 1px solid var(--color-border-hover);
+}
+.header__category {
+  display: flex;
+  gap: 10px;
+}
+.category__item {
+  background: var(--color-background-mute);
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 12px;
+}
+.main {
+  background: var(--color-background-mute);
+  padding: 5px 0 0 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr; /* Две колонки, каждая занимает 50% доступной ширины */
+  gap: 10px;
+}
+</style>
